cee846387c905b5e932336b38f546c3e43229acf
roy-ermers <dangergamesnl@gmail.com>
2018-03-21 15:53:29 +0100

    fixed security system

--- Hackathon/Hackathon/dashboard.aspx.cs
+++ Hackathon/Hackathon/dashboard.aspx.cs
@@ -9,6 +9,26 @@ public partial class dashboard : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+		var UserId = Sql.Query($"SELECT Id FROM [User] WHERE [Name] ='{Request.QueryString["UserName"]}';");
+		UserId.Read();
+		if (!HasAccess((int)UserId.GetValue(0)))
+			Response.Redirect($"/login.aspx?error=NO_ACCESS");
 
-    }
+	}
+	bool HasAccess(int DesiredUserID)
+	{
+		if (Context.Session["CurrentUser"] == null || Context.Session["SessionKey"] == null)
+			return false;
+		int CurrentUserID = int.Parse(Context.Session["CurrentUser"].ToString());
+		string SessionKey = Context.Session["SessionKey"].ToString();
+		if (DesiredUserID != CurrentUserID)
+			return false;
+		//check if the user is logged in
+
+		//then check if the user is legit
+		var query = Sql.ScalarQuery("SELECT UserId FROM SessionKey WHERE UserId = " + CurrentUserID + " AND SessionKey = '" + SessionKey + "';");
+
+		return query != null;
+	}
+
 }
