cccec8e1e3744eeabe86bc872001ef1104dca926
roy-ermers <dangergamesnl@gmail.com>
2018-03-21 21:28:04 +0100

    Added a way to add profile pictures

---
+++ Hackathon/Hackathon/App_Code/HackPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for HackPage
+/// </summary>
+public class HackPage : System.Web.UI.Page
+{
+
+	public HackPage()
+	{
+#pragma warning disable CS0618 // Type or member is obsolete
+		RegisterStartupScript("PostBackFix",
+  "<script>document.forms[0].action='';</script>");
+#pragma warning restore CS0618 // Type or member is obsolete
+	}
+}
--- Hackathon/Hackathon/dashboard.aspx.cs
+++ Hackathon/Hackathon/dashboard.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class dashboard : System.Web.UI.Page
+public partial class dashboard : HackPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
