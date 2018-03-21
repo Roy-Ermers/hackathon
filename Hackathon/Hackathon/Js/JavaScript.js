cd07d2a5176a9b79b45512c20db53d3b9560d7bf
JonasTH2001 <jonas.terhorst@edu-kw1c.nl>
2018-03-21 09:56:01 +0100

    header

---
+++ Hackathon/Hackathon/Js/JavaScript.js
@@ -0,0 +1,22 @@
+﻿if ($("#topper").length) {
+    $("main").css("margin-top", "0");
+    $("header").css("background", "transparent");
+}
+
+var scroll = 0;
+$(window).scroll(
+    function () {
+        var top = $(this).scrollTop();
+
+        if (top > scroll) {
+            if ($("#topper").length) {
+                $("header").css("background", "#2c3e50");
+            }
+        } else {
+            if ($("#topper").length) {
+                $("header").css("background", "transparent");
+            }
+        }
+        scroll = top;
+    }
+);
